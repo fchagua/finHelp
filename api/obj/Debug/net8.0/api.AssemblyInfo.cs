3386a097962a61f1b2ce4f0c7617f1da83672cfc
Francisco Chagua <fchagua@purdue.edu>
2024-11-12 16:20:53 -0600

    Add Portfolio GET via Portfolio Repo and Portfolio Controller

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea407ae47703f6f2cb6f5fcb3eda0bc9c6294a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b666cacdaee657702517c67e27e5f47f2b6a9b57")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
